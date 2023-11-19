bbd7e163a32c5c1220e09aa398b373f673f900d2
YiWun Jhang <kagamiakira8366@gmail.com>
2023-11-19 13:19:23 +0800

    202311191318_bgChange

--- js/global.js
+++ js/global.js
@@ -1,3 +1,10 @@
-import { AOS } from 'aos';
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init();
+});
 
-<script src="dist/aos.js"></script>
+const elem = document.querySelector('body');
+const scrolled = () => {
+    const threshold = document.documentElement.scrollTop > 10;
+    elem.classList.toggle('scrolled', threshold);
+};
+window.addEventListener('scroll', scrolled);
